1fca0b44015b74b320e2f38a338f88ca8295c81a
Freeman010 <fabi.sasse@web.de>
2024-03-07 11:24:03 +0100

    playermanager update notizen

---
+++ javascript/playermanager.js
@@ -0,0 +1,20 @@
+/*
+soll das aktuelle fenster updaten indem sich der spieler befindet
+z.b. die ressourcen die hochgezählt werden für den jeweiligen planeten
+ausserdem die aktuellen flottenbewegungen und nachrichten erfassen
+Der playermaneger muss wissen auf welchem planeten er sich befindet , damit
+die gebäude auf dem richtigem planeten gebaut werden
+soll funktionen aus allen benötiten scripten aufrufen können.
+schiff bauen 
+gebäude bauen 
+flotte versenden
+forschung starten etc
+
+daten die der playermaneger braucht:
+playerdata script
+
+benötigt zugriff auf funktionen zum gebäude bauen schiffe bauen und forschen(zugriff auf die jeweiligen skripte)
+benötigt funktion um einzuloggen
+benötigt funktion um planeten umzubenennen
+benötigt funktion um den spielernamen zu ändern
+*/ 
