2950964e40dd73753c778ba17b025bb0bac26af7
Marius <153908196+Mynchro@users.noreply.github.com>
2024-03-11 16:05:38 +0100

    werften.js hinzugefügt, html und css angepasst

---
+++ javascript/werften.js
@@ -0,0 +1,28 @@
+// Ändern der Title-Box innerhalb der Content Box bei Buttonclick (Vgl. forschung.js)
+
+const buttonKlein = document.getElementById("change-klein");
+const buttonMittel = document.getElementById("change-mittel");
+const buttonGross = document.getElementById("change-gross");
+const div = document.getElementById("btn-content");
+
+const kleineWerft =
+  '<button class="btn btn1">Kleiner Transporter</button>\n<button class="btn btn2">Leichter Jäger</button>\n<button class="btn btn3">Schwerer Jäger</button>\n<button class="btn btn4">Bomber</button>\n<button class="btn btn5">Fregatte</button>\n<button class="btn btn6">Mining Drone</button>\n<button class="btn btn7">Flaggeschütz</button>\n<button class="btn btn8">Lasergeschütz</button>';
+const mittlereWerft =
+  '<button class="btn btn1">Großer Transporter</button>\n<button class="btn btn2">Zerstörer</button>\n<button class="btn btn3">Kreuzer</button>\n<button class="btn btn4">Flugdeckkreuzer</button>\n<button class="btn btn5">Kolonieschiff</button>\n<button class="btn btn6">Bergbauschiff</button>\n<button class="btn btn7">Ionenkanone</button>\n<button class="btn btn8">Railgun</button>';
+const grosseWerft =
+  '<button class="btn btn9">Schlachtschiff</button>\n<button class="btn btn10">Trägerschiff</button>\n<button class="btn btn11">Schlachtkreuzer</button>\n<button class="btn btn12">Partikelgeschütz</button>';
+
+function changeContentMittel() {
+  div.innerHTML = mittlereWerft;
+}
+buttonMittel.addEventListener("click", changeContentMittel);
+
+function changeContentKlein() {
+  div.innerHTML = kleineWerft;
+}
+buttonKlein.addEventListener("click", changeContentKlein);
+
+function changeContentGross() {
+  div.innerHTML = grosseWerft;
+}
+buttonGross.addEventListener("click", changeContentGross);
