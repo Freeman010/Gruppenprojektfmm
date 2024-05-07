59b0398cb232fddee75df51a206439c542c7410b
freeman <fabian.sasse@dci-student.org>
2024-05-07 09:54:56 +0200

    fleet

--- kampfsystem/index.js
+++ kampfsystem/index.js
@@ -1,22 +1,6 @@
-/*
-1. Erstellen von vorgefertigten Flotten, die der Spieler sich auswählen kann
-2. Zwei Anfangsflotten aus denen gewählt werden kann */
-
-/*  Kleine Werft:   Kleiner Transporter, Leichter Jäger, Schwerer Jäger, Bomber, Fregatte
-    Mittlere Werft: Großer Transporter, Zerstörer, Kreuzer, Flugdeckkreuzer
-
-
 
 class Fleet {
-    fleetArray;
-    cargo;
-    constructor(fleet, cargo) {
-        this.cargo = cargo;
-        this.fleetArray = fleet;
-    }
-}
-class Fleet {
-    constructor(ships) {
+    constructor() {
 
     }
     maxCargo = 0;
@@ -31,3 +15,13 @@ class Fleet {
         }
     }
 }
+/*
+1. Erstellen von vorgefertigten Flotten, die der Spieler sich auswählen kann
+2. Zwei Anfangsflotten aus denen gewählt werden kann */
+
+/*  Kleine Werft:   Kleiner Transporter, Leichter Jäger, Schwerer Jäger, Bomber, Fregatte
+    Mittlere Werft: Großer Transporter, Zerstörer, Kreuzer, Flugdeckkreuzer
+
+
+
+
