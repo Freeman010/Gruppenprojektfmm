f545ac509426f25ed04a11418df5d3b27293ac14
freeman <fabian.sasse@dci-student.org>
2024-05-07 09:48:50 +0200

    fleet

--- kampfsystem/index.js
+++ kampfsystem/index.js
@@ -1,3 +1,24 @@
 class Fleet {
+    fleetArray;
+    cargo;
+    constructor(fleet, cargo) {
+        this.cargo = cargo;
+        this.fleetArray = fleet;
+    }
+}
+class Cargo {
+    constructor() {
 
+    }
+    maxCargo = 0;
+    actualCargo = 0;
+    ressources = 0;
+    ammo = 0;
+    setMaxCargo(fleet) {
+        if (Array.isArray(fleet)) {
+            const totalCargo = fleet.reduce((accumulator, unit) => {
+                return accumulator + unit.cargo;
+            }, 0);
+        }
+    }
 }
