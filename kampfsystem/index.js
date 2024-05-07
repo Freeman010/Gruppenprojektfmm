4e28d0e1963da0cd2fa58b5ba7cbb6d8e6f4f66a
Marius <webermarius1995@gmail.com>
2024-05-07 09:43:42 +0200

    unitdata update, index.js angefangen

--- kampfsystem/index.js
+++ kampfsystem/index.js
@@ -1,3 +1,10 @@
-class Fleet {
+/*
+1. Erstellen von vorgefertigten Flotten, die der Spieler sich auswählen kann
+2. Zwei Anfangsflotten aus denen gewählt werden kann */
 
-}
+/*  Kleine Werft:   Kleiner Transporter, Leichter Jäger, Schwerer Jäger, Bomber, Fregatte
+    Mittlere Werft: Großer Transporter, Zerstörer, Kreuzer, Flugdeckkreuzer
+
+
+
+
